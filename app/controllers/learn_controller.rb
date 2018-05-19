c24a984515fb030121e903eccfbd14171a5756f8
Renan Silva <renan.tssilva@gmail.com>
2018-05-19 16:24:09 -0300

    fix learn controller

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -1,6 +1,6 @@
 class LearnController < ApplicationController
   before_action :authenticate_user!
   def index
-    @posts = Post.where(:learning_language => LearningLanguage.where(:user => current_user.id).pluck(:language), :mother_language => MotherLanguage.where(:user => current_user.id).pluck(:language))
+    @posts = Post.where(:learning_language => LearningLanguage.where(:user => current_user.id), :mother_language => MotherLanguage.where(:user => current_user.id))
   end
 end
