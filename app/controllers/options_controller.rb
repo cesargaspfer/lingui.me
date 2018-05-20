b167e5fab0ea4891e3686bcccc0669bd4fa160e3
Renan Silva <renan.tssilva@gmail.com>
2018-05-19 22:39:28 -0300

    save state

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -5,22 +5,42 @@ class OptionsController < ApplicationController
     @user = current_user
     mother_languages = MotherLanguage.where(:user => @user)
     @mother_languages = Array.new
-    mother_languages.each  do |language|
-      @mother_languages.push(language)
+    mother_languages.each  do |idiom|
+      @mother_languages.push(idiom.language)
     end
     learning_languages = LearningLanguage.where(:user => @user)
     @learning_languages = Array.new
-    learning_languages.each do |language|
-      @learning_languages.push(language)
+    learning_languages.each do |idiom|
+      @learning_languages.push(idiom.language)
     end
     @available_languages = Language.all
   end
 
   def create
-    
+    # @count = 0
+    # @available_languages.each do |language|
+    #   if(params["teach#{@count}".to_sym])
+    #     if !@mother_languages.include? language
+    #       add_mother_language language
+    #     end
+    #   else
+    #     if @mother_languages.include? language
+    #       remove_mother_language language
+    #     end
+    #   end
+    # end
   end
 
-  def add_learning_language(language)
-
-  end
+  # def add_mother_language(language)
+  #   @user = User.find_by(:id => current_user.id)
+  #   @language = Language.find_by(:id => language.id)
+  #   newLanguage = MotherLanguage.new(user: @user, language: @language)
+  #   newLanguage.save!
+  # end
+  # def remove_mother_language(language)
+  #   @user = User.find_by(:id => current_user.id)
+  #   @language = Language.find_by(:id => language.id)
+  #   newLanguage = MotherLanguage.new(user: @user, language: @language)
+  #   newLanguage.save!
+  # end
 end
