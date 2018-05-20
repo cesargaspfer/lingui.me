6436d320966a021b00e08010c7e3174c1f8bce59
Renan Silva <renan.tssilva@gmail.com>
2018-05-19 22:02:13 -0300

    add options

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,11 +1,31 @@
 class OptionsController < ApplicationController
   before_action :authenticate_user!
 
-  def options
+  def index
     @user = current_user
-    @user_mother_languages = LearningLanguage.find_by(:user => @user)
-    @user_learning_languages = LearningLanguage.where(:user => @user)
+    mother_languages = MotherLanguage.where(:user => @user)
+    @mother_languages = Array.new
+    mother_languages.each  do |language|
+      @mother_languages.push(language)
+    end
+    learning_languages = LearningLanguage.where(:user => @user)
+    @learning_languages = Array.new
+    learning_languages.each do |language|
+      @learning_languages.push(language)
+    end
     @available_languages = Language.all
+  end
+
+  def create
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def add_learning_language(language)
