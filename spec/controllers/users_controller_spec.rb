8d00a6f34f1bfa227d8deceadb610abbfe75a946
Renan Silva <renan.tssilva@gmail.com>
2018-06-08 15:54:42 -0300

    add user controller test Co-authored-by: Igor igorfratelsantana@gmail.com

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+RSpec.describe UserController, type: :controller do
+  describe "GET index" do
+    it "should return 200 when user logged in" do
+      sign_in create(:user)
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    it "should redirect when user not logged in" do
+      get :index
+      expect(response.status).to eq(302)
+    end
+
+    it "should redirect to log in page when user not logged in" do
+      get :index
+      expect(redirect_to new_user_session_path)
+    end
+  end
+end
